de5638729db96bade715f1c88fff85514aea2ca0
Adrian Joseph <adrianignatius15@hotmail.com>
2025-10-02 21:44:45 +0800

    Created Write & Read database contexts

---
+++ Data/ReadDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ReadDbContext : DbContext
+{
+    public ReadDbContext(DbContextOptions<ReadDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<Order> Orders { get; set; } = null!;
+}
